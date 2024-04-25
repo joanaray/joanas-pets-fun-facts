3cc697057458983907d78c0fcb81ddeeafa48bc6
Joana <account@joana.cc>
2024-04-25 02:01:13 +0100

    Header and Footer components
    Updated Header and Footer and moved them to the components folder

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+      <footer>
+        <div className="content small">
+          <p><a href="https://github.com/joanaray/joanas-pets-fun-facts">GitHub Repo</a> &bull; <a href="http://www.joana.cc">Joana Ray</a> &bull; 2024</p>
+        </div>
+      </footer>
+  );
+}
+export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ function Header() {
       <header>
         <div className="content">
           <hgroup>
-            <h1>Joana's Pets Fun Facts</h1>
+            <h1>Joana's Pets' Fun Facts</h1>
             <p>I have pets and you should know about them.</p>
           </hgroup>
         </div>
